ea9efe34475ab54ba9ee05e75215a86d5f64b745
Chloe Fox <cfox_01@arcadia.edu>
2023-12-05 20:32:18 -0500

    UI Updates for playlist generation

---
+++ vibecraft/components/PlaylistPage.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import { View, StyleSheet } from 'react-native';
+import { SearchBar } from 'react-native-elements';
+import { LinearGradient } from 'expo-linear-gradient';
+
+const PlaylistPage = () => {
+  const [searchText, setSearchText] = useState('');
+
+  const handleSearch = (text) => {
+    // Handle your search logic here
+    setSearchText(text);
+    // For example, you can filter a list based on searchText
+    // Update your filtered data or trigger an API call with the text
+  };
+
+  return (
+    <LinearGradient
+      colors={[
+        'rgba(212,173,249,1)',
+        'rgba(113,0,218,1)',
+        'rgba(0,0,0,1)',
+      ]}
+      start={{ x: 0, y: 0 }}
+      end={{ x: 1, y: 1 }}
+      style={styles.container}
+    >
+      <SearchBar
+        placeholder="Artist Name Here..."
+        onChangeText={handleSearch}
+        value={searchText}
+      />
+    </LinearGradient>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 10,
+    paddingTop: 20,
+  },
+});
+
+export default PlaylistPage;
--- vibecraft/components/homepage.js
+++ vibecraft/components/homepage.js
@@ -1,28 +1,83 @@
-import React, { useState } from 'react';
-import { View } from 'react-native';
-import { SearchBar } from 'react-native-elements';
+import React from 'react';
+import { View, Text, Image, TouchableOpacity, StyleSheet } from 'react-native';
 
-const HomePage = () => {
-  const [searchText, setSearchText] = useState('');
-
-  const handleSearch = (text) => {
-    // Handle your search logic here
-    setSearchText(text);
-    // For example, you can filter a list based on searchText
-    // Update your filtered data or trigger an API call with the text
+const HomePage = ({ navigation }) => {
+  const handleButtonClick = () => {
+    // Navigate to the "Playlist" page when the button is clicked
+    navigation.navigate('Playlist');
   };
 
   return (
-    <View>
-      <SearchBar
-        placeholder="Type Here..."
-        onChangeText={handleSearch}
-        value={searchText}
-      />
-      {/* Your other UI components */}
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Image source={require('../assets/circle.png')} style={styles.logo} />
+        <Text style={styles.headerText}>User Name</Text>
+      </View>
+      <View style={styles.content}>
+        <TouchableOpacity style={styles.button} onPress={handleButtonClick}>
+          <Text style={styles.buttonText}>+ Generate Playlist</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+    backgroundColor: '#242424',
+  },
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 20,
+    marginTop: 80,
+    marginRight: 200,
+  },
+  logo: {
+    width: 50, // Set the width of your logo
+    height: 50, // Set the height of your logo
+    marginRight: 20,
+  },
+  headerText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+  content: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  button: {
+    backgroundColor: '#01C092',
+    paddingVertical: 15,
+    paddingHorizontal: 20,
+    borderRadius: 10,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+    shadowOpacity: 0.3,
+    shadowRadius: 4.65,
+    elevation: 6,
+    // Add a linear gradient for a glossy effect
+    backgroundGradient: {
+      colors: ['#2AC940', '#219C30'],
+      start: { x: 0.5, y: 0 },
+      end: { x: 0.5, y: 1 },
+    },
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+    textAlign: 'center',
+  },
+});
+
 export default HomePage;
+
 
