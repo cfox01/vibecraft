14f7b70a87c987e67fdee7543359e7fa2cdd5924
tedkorolishin <tkorolishin@arcadia.edu>
2023-12-06 18:48:35 -0500

    non-working search bar outline

--- vibecraft/components/PlaylistPage.js
+++ vibecraft/components/PlaylistPage.js
@@ -1,17 +1,75 @@
 import React, { useState } from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Alert, Text, Image } from 'react-native'; // Add necessary imports
 import { SearchBar } from 'react-native-elements';
 import { LinearGradient } from 'expo-linear-gradient';
+import { getAccessToken } from '../auth'; // Make sure this function fetches the access token properly
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 10,
+    paddingTop: 20,
+  },
+  resultsContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+    marginTop: 20,
+  },
+  artistCard: {
+    alignItems: 'center',
+    margin: 10,
+  },
+  artistImage: {
+    width: 150,
+    height: 150,
+    borderRadius: 75,
+  },
+  artistName: {
+    marginTop: 10,
+    fontSize: 16,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  // ... add more styles as needed
+});
 
 const PlaylistPage = () => {
-  const [searchText, setSearchText] = useState('Guest');
+  const [searchText, setSearchText] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
 
-  const handleSearch = (text) => {
-    // Handle your search logic here
-    setSearchText(text);
-    // For example, you can filter a list based on searchText
-    // Update your filtered data or trigger an API call with the text
+  const handleSearch = async (text) => {
+    try {
+      setSearchText(text); // Update search text
+
+      const accessToken = await getAccessToken(); // Fetch the access token
+
+      const response = await fetch(`https://api.spotify.com/v1/search?q=${encodeURIComponent(text)}&type=artist`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error('Network response was not ok.');
+      }
+
+      const data = await response.json();
+      console.log(data);
+      setSearchResults(data.artists.items || []); // Update search results or set to empty array if no items
+    } catch (error) {
+      console.error('Error fetching artist data:', error);
+      Alert.alert('Error', 'Failed to fetch artist data. Please try again.');
+    }
+  };
+
+  const handleKeyPress = ({ nativeEvent }) => {
+    if (nativeEvent.key === 'Enter') {
+      console.log("Pressed Enter");
+      handleSearch(searchText);
+    }
   };
 
   return (
@@ -27,19 +85,25 @@ const PlaylistPage = () => {
     >
       <SearchBar
         placeholder="Artist Name Here..."
-        onChangeText={handleSearch}
+        onChangeText={setSearchText}
         value={searchText}
+        onKeyPress={handleKeyPress}
       />
+  
+      {/* Display the search results */}
+      <View style={styles.resultsContainer}>
+        {searchResults.map((artist) => (
+          <View key={artist.id} style={styles.artistCard}>
+            <Text style={styles.artistName}>{artist.name}</Text>
+            {/* Add more artist information as needed */}
+          </View>
+        ))}
+      </View>
     </LinearGradient>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingHorizontal: 10,
-    paddingTop: 20,
-  },
-});
 
 export default PlaylistPage;
+
+
